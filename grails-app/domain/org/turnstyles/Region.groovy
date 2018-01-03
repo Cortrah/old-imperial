42b44de35f92a561aab5b2c2bb96fb5fcc808ebb
Cortlandt Winters <cort@cortwinters.net>
2018-01-03 07:32:56 -0500

    add some more domain classes for turns and turn statuses, but still pretty rudimentary and without relationships

--- grails-app/domain/org/turnstyles/Region.groovy
+++ grails-app/domain/org/turnstyles/Region.groovy
@@ -10,10 +10,9 @@ class Region {
     String code;
     String label;
     String description = "default description";
-
     String  notes = "notes";
-    String  status = "?";
-    Integer resistance = 3;
+
+    boolean bordersVolcano;
 
     double x = 0;
     double y = 0;
@@ -23,21 +22,8 @@ class Region {
     double cY = 50;
     String path = "m0,0 L100,0 L100,100 L0,100 z";
 
-    boolean isSecret;
-    boolean hasRoad;
-    boolean bordersVolcano;
-    boolean isInimical;
-
-    Integer gp = 1;
-    Integer ap = 1;
-    Integer nfp = 1;
-    Integer mana = 1;
-    double  pwb = 1.0;
-    double  tv = 1.0;
-
     static belongsTo = [majorMap: MajorMap];
     static hasMany = [locations: Location];
-    TerrainType terrainType;
 
     String toString() { "$name"}
 
@@ -48,23 +34,9 @@ class Region {
         code blank: false
         label blank: false
         description nullable: true
-        terrainType nullable: true // ToDo: init to clear terrain
-
         notes blank: true
-        status inList: ['?','free', 'pacified', 'tributary', 'allied', 'friendly', 'homeland']
-        resistance range: 1..10
 
-        gp()
-        ap()
-        nfp()
-        mana()
-        pwb()
-        tv()
-
-        isSecret()
-        hasRoad()
         bordersVolcano()
-        isInimical()
 
         x()
         y()
