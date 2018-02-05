f79f5ce602867405934228ab4ce1884d69e3035a
Cortlandt Winters <cort@cortwinters.net>
2018-02-05 07:57:48 -0500

    Add Spells and Spelltypes

---
+++ grails-app/domain/org/turnstyles/SpellType.groovy
@@ -0,0 +1,46 @@
+package org.turnstyles
+
+class SpellType {
+
+    String name = "Name"
+    String code = "Code"
+    String description = ""
+
+    String level = "Minor"
+    Integer nsr = 4
+    String whenToCast = "Any"
+    Integer timeToCast = 0
+
+    Boolean allowsMultipleCasters = false
+
+    Integer initialManaCost = 0
+    Integer perUnitManaCost = 0
+    Integer totalManaCost = 0
+
+    Boolean isSecret = false
+
+    static hasMany = [spells :SpellType]
+
+    // Effect
+
+    String toString() {"$name"}
+
+    static graphql = true
+    static constraints = {
+        name blank: false
+        code blank: false
+        description nullable: true
+
+        level()
+        nsr()
+        whenToCast()
+        timeToCast()
+
+        allowsMultipleCasters()
+        initialManaCost()
+        perUnitManaCost()
+        totalManaCost()
+
+        isSecret()
+    }
+}
---
+++ grails-app/domain/org/turnstyles/SpellsReligion.groovy
@@ -0,0 +1,13 @@
+package org.turnstyles
+
+class SpellsReligion {
+
+    SpellType spellType
+    Religion religion
+
+    String toString() {"$name"}
+
+    static graphql = true
+    static constraints = {
+    }
+}
