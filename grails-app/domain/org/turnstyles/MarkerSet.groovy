8b38f0ca580c36ee30508a977e5b6119883644de
Cortlandt Winters <cort@cortwinters.net>
2018-02-19 12:17:37 -0500

    Add markers and markersets, allow sharing, but not full phase data yet, and annotate

--- grails-app/domain/org/turnstyles/MarkerSet.groovy
+++ grails-app/domain/org/turnstyles/MarkerSet.groovy
@@ -1,5 +1,11 @@
 package org.turnstyles
 
+// This version allows sharing of freeform markers amongst groups of realms and is meant for annotation and planning
+// it is multiturn but does not include phase data yet like cartographer did
+// perhaps add drawing tools
+// Tokens are similar but game managed things, markers cant be used to target locations
+// though that might be a good thing to tie together
+
 class MarkerSet {
     String name = "Name"
     Integer color = 0
