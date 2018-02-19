db805f2bc6b08ddad2d33ca2e26e622d782059ea
Cortlandt Winters <cort@cortwinters.net>
2018-02-19 12:20:55 -0500

    just thinking about markers, markersets and tokens

--- grails-app/domain/org/turnstyles/MarkerSet.groovy
+++ grails-app/domain/org/turnstyles/MarkerSet.groovy
@@ -3,8 +3,11 @@ package org.turnstyles
 // This version allows sharing of freeform markers amongst groups of realms and is meant for annotation and planning
 // it is multiturn but does not include phase data yet like cartographer did
 // perhaps add drawing tools
+
 // Tokens are similar but game managed things, markers cant be used to target locations
 // though that might be a good thing to tie together
+// with default marketsets for leaders items troops and user created region locations
+// though then we should tie them to regions instead of majorMaps
 
 class MarkerSet {
     String name = "Name"
