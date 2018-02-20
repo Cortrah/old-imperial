8a55e1b7ae27fa94e0bd790b1afb4d26f8f2915e
Cortlandt Winters <cort@cortwinters.net>
2018-02-20 06:50:05 -0500

    add realmType

---
+++ grails-app/domain/org/turnstyles/RealmType.groovy
@@ -0,0 +1,18 @@
+package org.turnstyles
+
+// primacy, elector, dutchy , secret society ...
+
+class RealmType {
+
+    String name = "Name"
+    String code = "Code"
+    String description = ""
+    String toString() {"$name"}
+
+    static graphql = true
+    static constraints = {
+        name blank: false
+        code blank: false
+        description nullable: true
+    }
+}
